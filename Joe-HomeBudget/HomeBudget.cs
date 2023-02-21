afbb8dff450f8093ff1b76f0d38907199ec4048f
Yensan <Streykidash@gmail.com>
2023-02-20 20:29:46 -0500

    -Added new test from lea -Fixed up to 52 bug from test -Added updatePropretie method to add list and passed the test -Finalized update to database method + documentation -Remove previous created test for update and add -Created a third add method without using category as parameter -added Categories contructor to set category to default if newDB -Modifyed retrieve Categories from Database for it to work

--- Joe-HomeBudget/HomeBudget.cs
+++ Joe-HomeBudget/HomeBudget.cs
@@ -124,18 +124,19 @@ namespace Budget
             {
                 Database.newDatabase(databaseFile);
                 newDB = true;
+                DBCategoryType(Database.dbConnection);
             }
 
             // create the category object
-            _categories = new Categories(Database.dbConnection, newDB);
+            //_categories = new Categories(Database.dbConnection, newDB);
 
             // create the _expenses course
 
-            DBCategoryType(Database.dbConnection);
+           
             _categories = new Categories();
             _expenses = new Expenses();
 
-            //_expenses.ReadFromFile(expensesXMLFile);
+            _expenses.ReadFromFile(expensesXMLFile);
         }
 
         private void DBCategoryType(SQLiteConnection db)
@@ -505,14 +506,14 @@ namespace Budget
                 }
 
                 // keep track of running totals
-                total = total - queryResult.Amount;
+                total = total + queryResult.Amount;
                 items.Add(new BudgetItem
                 {
                     CategoryID = queryResult.CatId,
                     ExpenseID = queryResult.ExpId,
                     ShortDescription = queryResult.Description,
                     Date = queryResult.Date,
-                    Amount = -queryResult.Amount,
+                    Amount = queryResult.Amount,
                     Category = queryResult.Category,
                     Balance = total
                 });
