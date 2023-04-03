54dfdb630aafdd79611d69e9da5224623ac97df1
sebg-dev <gorsebo@gmail.com>
2023-04-03 13:12:21 -0400

    Added reference to HomeBudget DLL.

--- JoeWpfHomeBudget/Presenter.cs
+++ JoeWpfHomeBudget/Presenter.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Budget;
 
 namespace JoeWpfHomeBudget
 {
     class Presenter
     {
+        private readonly ViewInterface view;
+        private readonly HomeBudget HomeBudget;
+        public Presenter(ViewInterface v)
+        {
+            HomeBudget = new HomeBudget();
+            view = v;
+        }
     }
 }
