bf53ea299e6774a61b06929344e5d88e03670f8d
sebg-dev <44349956+sebg-dev@users.noreply.github.com>
2023-04-22 01:03:21 -0400

    Made an interface and added a few things to the back end.

--- JoeWpfHomeBudget/Update_Delete_Budget_Item.xaml.cs
+++ JoeWpfHomeBudget/Update_Delete_Budget_Item.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +12,122 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Budget;
 
 namespace JoeWpfHomeBudget
 {
     /// <summary>
     /// Interaction logic for Update_Delete_Budget_Item.xaml
     /// </summary>
-    public partial class Update_Delete_Budget_Item : Window
+    public partial class Update_Delete_Budget_Item : Window, Update_Delete_Interface
     {
-        public Update_Delete_Budget_Item()
+        private readonly Presenter presenter;
+        private Boolean submitted;
+        private Boolean cancelled;
+
+        public Update_Delete_Budget_Item(Presenter _presenter)
         {
+
             InitializeComponent();
+
+            presenter = _presenter;
+            submitted = false;
+            cancelled = false;
+
+            SetDateDefault();
+            PopulateCategoryInBox();
+
+        }
+
+        private void update_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            CancelExpense();
+        }
+        public void ShowError(String err)
+        {
+            MessageBox.Show(err, "Invalid input", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        public void ShowValid(string message)
+        {
+            MessageBox.Show(message, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        public void ClearExpense()
+        {
+            SetDateDefault();
+            amount_expense.Clear();
+            categoryList.SelectedIndex = -1;
+            description.Clear();
+        }
+
+        public void SetDateDefault()
+        {
+
+            date_expense.DisplayDate = DateTime.Now;
+            date_expense.SelectedDate = DateTime.Now;
+
+        }
+
+        public void PopulateCategoryInBox()
+        {
+            List<Category> categories = presenter.GetAllCategories();
+
+            foreach (Category category in categories)
+            {
+                categoryList.Items.Add(category.Description);
+
+
+            }
+        }
+        public void CancelExpense()
+        {
+
+            if (MessageBox.Show("Do you really want to cancel updating your Expense", "Cancel", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                cancelled = true;
+                ClearExpense();
+            }
+
+        }
+        void SaveChangesValidationBeforeClosing(object sender, CancelEventArgs e)
+        {
+            // If user did not save changes, notify user and ask for a response            
+            if (!submitted && !cancelled)
+            {
+                if ((amount_expense.Text != string.Empty || description.Text != string.Empty) ||
+                    (categoryList.SelectedIndex != -1 && (amount_expense.Text == string.Empty || description.Text == string.Empty)))
+                {
+                    if (presenter.SaveBeforeClosing())
+                    {
+                        e.Cancel = false;
+                    }
+                    else
+                    {
+                        e.Cancel = true;
+                    }
+                }
+            }
+        }
+
+        private void add_Category_Click(object sender, RoutedEventArgs e)
+        {
+
+                AddCategory addCategory = new AddCategory(presenter);
+                addCategory.ShowDialog();
+                PopulateCategoryInBox();
+
+
+
+                
         }
     }
 }
---
+++ JoeWpfHomeBudget/Update_Delete_Interface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoeWpfHomeBudget
+{
+    interface Update_Delete_Interface
+    {
+        void SetDateDefault();
+        void PopulateCategoryInBox();
+    }
+}
